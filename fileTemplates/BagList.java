94a41d8af9df84a941bd2d117c428066d27f8365
mariemsaleh <mariem.saleh@hotmail.com>
2023-01-20 08:36:55 +0100

    IC-2022.2.4 <mariemsaleh@Mariems-MacBook-Pro.local Create baseRefactoring.xml, Main.java, ide.general.xml, advancedSettings.xml, diff.xml, BagList.java, shared-indexes.xml, Default.xml, find.xml, DontShowAgainFeedbackService.xml, filetypes.xml, editor-font.xml, colors.scheme.xml, ShoeList.java, github.xml, BankomatTest.zip, laf.xml, jdk.table.xml, maven.wrapper.mapping.xml, vcs.xml, toStringTemplates.xml, OnlinShop..zip, editor.xml, debugger.xml, setterTemplates.xml, updates.xml

---
+++ fileTemplates/BagList.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ${NAME} {
+
+
+    static List<String> väska;
+    static {
+
+        väska = new ArrayList<String>();
+
+        väska.add("gymväskor   Är 750kr");
+        väska.add("\n Ryggsäckor  Är 600 ");
+        väska.add("\n Skinnväskor Är 1000");
+        väska.add("\n Handväskor  Är 450");
+        System.out.println("Väskor ");
+
+
+    }
+
+}
---
+++ fileTemplates/ShoeList.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ${NAME} {
+
+    static List<String> Skor;
+    static {
+        Skor = new ArrayList<String>();
+        Skor.add("Sneakers         Är 950kr");
+        Skor.add("\n platta skor     Är 500kr ");
+        Skor.add("\n Stövlar         Är 480kr");
+        Skor.add("\n Högklackar skor Är  1100kr");
+
+}
+
+}
+
